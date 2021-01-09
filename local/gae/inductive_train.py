d063158bc420e6bfac7b3c84f5b201e0d35c2184
xiaozhuoyue <xiaozhuoyue@momenta.ai>
2021-01-09 17:06:30 +0800

    developing inductive GAE..

--- local/gae/inductive_train.py
+++ local/gae/inductive_train.py
@@ -51,110 +51,6 @@ name_str = FLAGS.name
 start_time = time.time()
 
 
-def gae_for_na(name):
-    """
-    train and evaluate YUTAO results for a specific name
-    :param name:  author name
-    :return: evaluation results
-    """
-    adj, features, labels = load_local_data(exp_name, IDF_THRESHOLD, name=name)
-
-    # Store original adjacency matrix (without diagonal entries) for later
-    adj_orig = adj
-    adj_orig = adj_orig - sp.dia_matrix((adj_orig.diagonal()[np.newaxis, :], [0]), shape=adj_orig.shape)
-    adj_orig.eliminate_zeros()
-    adj_train = gen_train_edges(adj)
-
-    adj = adj_train
-
-    # Some preprocessing
-    adj_norm = preprocess_graph(adj)
-    num_nodes = adj.shape[0]
-    input_feature_dim = features.shape[1]
-    if FLAGS.is_sparse:  # TODO to test
-        # features = sparse_to_tuple(features.tocoo())
-        # features_nonzero = features[1].shape[0]
-        features = features.todense()  # TODO
-    else:
-        features = normalize_vectors(features)
-
-    # Define placeholders
-    placeholders = {
-        # 'features': tf.sparse_placeholder(tf.float32),
-        'features': tf.placeholder(tf.float32, shape=(None, input_feature_dim)),
-        'adj': tf.sparse_placeholder(tf.float32),
-        'adj_orig': tf.sparse_placeholder(tf.float32),
-        'dropout': tf.placeholder_with_default(0., shape=()),
-        'pos_weight': tf.sparse_placeholder(tf.float32),
-        'norm': tf.sparse_placeholder(tf.float32),
-    }
-
-    # Create model
-    model = None
-    if model_str == 'gcn_ae':
-        model = GCNModelAE(placeholders, input_feature_dim)
-    elif model_str == 'gcn_vae':
-        model = GCNModelVAE(placeholders, input_feature_dim, num_nodes)
-    pos_weight = float(adj.shape[0] * adj.shape[0] - adj.sum()) / adj.sum()  # negative edges/pos edges
-    print('positive edge weight', pos_weight)
-    norm = adj.shape[0] * adj.shape[0] / float((adj.shape[0] * adj.shape[0] - adj.nnz) * 2)
-
-    # Optimizer
-    with tf.name_scope('optimizer'):
-        if model_str == 'gcn_ae':
-            opt = OptimizerAE(preds=model.reconstructions,
-                              labels=tf.reshape(tf.sparse_tensor_to_dense(placeholders['adj_orig'],
-                                                                          validate_indices=False), [-1]),
-                              pos_weight=pos_weight,
-                              norm=norm)
-        elif model_str == 'gcn_vae':
-            opt = OptimizerVAE(preds=model.reconstructions,
-                               labels=tf.reshape(tf.sparse_tensor_to_dense(placeholders['adj_orig'],
-                                                                           validate_indices=False), [-1]),
-                               model=model, num_nodes=num_nodes,
-                               pos_weight=pos_weight,
-                               norm=norm)
-
-    # Initialize session
-    sess = tf.Session()
-    sess.run(tf.global_variables_initializer())
-
-    adj_label = adj_train + sp.eye(adj_train.shape[0])
-    adj_label = sparse_to_tuple(adj_label)
-
-    def get_embs():
-        feed_dict.update({placeholders['dropout']: 0})
-        emb = sess.run(model.z_mean, feed_dict=feed_dict)  # z_mean is better
-        return emb
-
-    # Train model
-    for epoch in range(FLAGS.epochs):
-        for batch in range(train_name_list):
-            t = time.time()
-            # Construct feed dictionary
-            feed_dict = construct_feed_dict(adj_norm, adj_label, features, placeholders)
-            feed_dict.update({placeholders['dropout']: FLAGS.dropout})
-            # Run single weight update
-            outs = sess.run([opt.opt_op, opt.cost, opt.accuracy],
-                            feed_dict=feed_dict)
-
-            # Compute average loss
-            avg_cost = outs[1]
-            avg_accuracy = outs[2]
-
-        print("Epoch:", '%04d' % (epoch + 1), "train_loss=", "{:.5f}".format(avg_cost),
-              "train_acc=", "{:.5f}".format(avg_accuracy),
-              "time=", "{:.5f}".format(time.time() - t))
-
-    emb = get_embs()
-    n_clusters = len(set(labels))
-    emb_norm = normalize_vectors(emb)
-    clusters_pred = clustering(emb_norm, num_clusters=n_clusters)
-    tp, fp, fn, prec, rec, f1 = pairwise_precision_recall_f1(clusters_pred, labels)
-    print('pairwise precision', '{:.5f}'.format(prec),
-          'recall', '{:.5f}'.format(rec),
-          'f1', '{:.5f}'.format(f1))
-    return [tp, fp, fn], [prec, rec, f1], num_nodes, n_clusters
 
 def load_local_preprocess_result(exp_name, IDF_THRESHOLD, name):
     path = join(settings.get_data_dir(exp_name), 'local', 'preprocess-{}'.format(IDF_THRESHOLD), name)
@@ -179,7 +75,9 @@ def train():
         'features': tf.placeholder(tf.float32, shape=(None, input_feature_dim)),
         'adj': tf.sparse_placeholder(tf.float32),
         'adj_orig': tf.sparse_placeholder(tf.float32),
-        'dropout': tf.placeholder_with_default(0., shape=())
+        'dropout': tf.placeholder_with_default(0., shape=()),
+        'pos_weight': tf.sparse_placeholder(tf.float32),
+        'norm': tf.sparse_placeholder(tf.float32),
     }
 
     # Create model
@@ -240,46 +138,6 @@ def train():
     #       'recall', '{:.5f}'.format(rec),
     #       'f1', '{:.5f}'.format(f1))
     # return [tp, fp, fn], [prec, rec, f1], num_nodes, n_clusters
-
-
-
-def main():
-    train_names, _ = settings.get_split_name_list(train_dataset_name)
-    _, test_names = settings.get_split_name_list(test_dataset_name)
-
-    wf = codecs.open(join(settings.get_out_dir(exp_name), 'local_clustering_results.csv'), 'w', encoding='utf-8')
-    wf.write('name,n_pubs,n_clusters,precision,recall,f1\n')
-    metrics = np.zeros(3)
-    cnt = 0
-    tp_fp_fn_sum = np.zeros(3)
-
-
-
-    for name in names:
-        tp_fp_fn, cur_metric, num_nodes, n_clusters = gae_for_na(name)
-        wf.write('{0},{1},{2},{3:.5f},{4:.5f},{5:.5f},{6:.5f},{7:.5f},{8:.5f},\n'.format(
-            name, num_nodes, n_clusters, cur_metric[0], cur_metric[1], cur_metric[2], *tp_fp_fn))
-        wf.flush()
-        for i, m in enumerate(cur_metric):
-            metrics[i] += m
-        cnt += 1
-        tp_fp_fn_sum += np.array(tp_fp_fn)
-        macro_prec = metrics[0] / cnt
-        macro_rec = metrics[1] / cnt
-        macro_f1 = cal_f1(macro_prec, macro_rec)
-        print('average until now', [macro_prec, macro_rec, macro_f1])
-        time_acc = time.time() - start_time
-        print(cnt, 'names', time_acc, 'avg time', time_acc / cnt)
-    macro_prec = metrics[0] / cnt
-    macro_rec = metrics[1] / cnt
-    macro_f1 = cal_f1(macro_prec, macro_rec)
-    tp, fp, fn = tp_fp_fn_sum
-    micro_precision = tp / (tp + fp)
-    micro_recall = tp / (tp + fn)
-    micro_f1 = 2 * micro_precision * micro_recall / (micro_precision + micro_recall)
-    wf.write('average,,,{0:.5f},{1:.5f},{2:.5f},{3:.5f},{4:5f},{5:5f}\n'.format(
-        macro_prec, macro_rec, macro_f1, micro_precision, micro_recall, micro_f1))
-    wf.close()
 
 
 if __name__ == '__main__':
