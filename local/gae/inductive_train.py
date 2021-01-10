9fc31f80a35e7d7aae23a867ca8f039be56fbf39
xiaozhuoyue <xiaozhuoyue@momenta.ai>
2021-01-10 19:21:08 +0800

    developing inductive GAE..

--- local/gae/inductive_train.py
+++ local/gae/inductive_train.py
@@ -76,7 +76,7 @@ def train():
         'adj': tf.sparse_placeholder(tf.float32),
         'adj_orig': tf.sparse_placeholder(tf.float32),
         'dropout': tf.placeholder_with_default(0., shape=()),
-        'pos_weight': tf.sparse_placeholder(tf.float32),
+        'pos_weight': tf.sparse_placeholder(tf.float32, shape=()),
         'norm': tf.sparse_placeholder(tf.float32),
     }
 
